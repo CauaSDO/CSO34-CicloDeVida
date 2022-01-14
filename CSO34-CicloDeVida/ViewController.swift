9a1990534e19bdfc7d3b786b16968667bb2a9a21
CauaSDO <cauasilva@icloud.com>
2022-01-14 17:02:33 -0300

    Initial Commit

---
+++ CSO34-CicloDeVida/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CSO34-CicloDeVida
+//
+//  Created by caua on 14/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
